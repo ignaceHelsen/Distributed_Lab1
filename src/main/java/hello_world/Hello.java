8f009e3a6d05a35cc5fc5af1d2b82617331294f5
Ignace Helsen <helsen.ignace@outlook.com>
2022-03-09 11:48:53 +0100

    MC4

--- src/main/java/hello_world/Hello.java
+++ src/main/java/hello_world/Hello.java
@@ -2,7 +2,6 @@ package hello_world;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World4.");
-        System.out.println("ROLLBACK");
+        System.out.println("Hello World.");
     }
 }

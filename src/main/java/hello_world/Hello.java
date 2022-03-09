9d7b29b8d0997680e8b166eaeba3d1efe899c9b5
Ignace Helsen <helsen.ignace@outlook.com>
2022-03-09 11:45:43 +0100

    Merge conflict

--- src/main/java/hello_world/Hello.java
+++ src/main/java/hello_world/Hello.java
@@ -2,6 +2,7 @@ package hello_world;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World.");
+        System.out.println("Hello World2.");
+        System.out.println("ROLLBACK");
     }
 }
